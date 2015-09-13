df8f7c032a9cf635665f7dea414ffb22b27f2e1d
Jakub Žitný <jzitny@oldanygroup.cz>
2015-09-14 00:38:57 +0200

    fixed hover bubbles

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,6 +4,7 @@ var React = require('react');
 
 var Menu = require('./Menu');
 var Dot = require('./Dot');
+var data = require('./../data');
 
 var Content = React.createClass({
 
@@ -45,15 +46,15 @@ var Content = React.createClass({
 						<div className="wave-green-large"></div>
 					</div>
 					<div className="row cell cell-20 center-block">
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="a" dataMenu="water" color="blue" marginTop="-15px" marginLeft="45px" />
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="b" dataMenu="island" color="green" marginTop="-15px" marginLeft="15px" />
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="c" dataMenu="water" color="blueish" marginTop="35px" marginLeft="-18px" />
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="d" dataMenu="place" color="red" marginTop="25px" marginLeft="35px" />
+						<Dot data={data["a"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="a" dataMenu="water" color="blue" marginTop="-15px" marginLeft="45px" />
+						<Dot data={data["b"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="b" dataMenu="island" color="green" marginTop="-15px" marginLeft="15px" />
+						<Dot data={data["c"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="c" dataMenu="water" color="blueish" marginTop="35px" marginLeft="-18px" />
+						<Dot data={data["d"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="d" dataMenu="place" color="red" marginTop="25px" marginLeft="35px" />
 					</div>
 					<div className="row cell cell-20"></div>
 					<div className="row cell cell-10 center-block">
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="e" dataMenu="island" color="beige" marginTop="25px" marginLeft="35px" />
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="f" dataMenu="island" color="brown" marginTop="0px" marginLeft="0px" />
+						<Dot data={data["e"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="e" dataMenu="island" color="beige" marginTop="25px" marginLeft="35px" />
+						<Dot data={data["f"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="f" dataMenu="island" color="brown" marginTop="0px" marginLeft="0px" />
 					</div>
 					<div className="row cell cell-10"></div>
 				</div>
@@ -62,11 +63,11 @@ var Content = React.createClass({
 					<div className="row cell cell-20"></div>
 					<div className="row cell cell-20"></div>
 					<div className="row cell cell-10 center-block">
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="g" dataMenu="place" color="darkgreen" marginTop="-45px" marginLeft="10px" />
+						<Dot data={data["g"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="g" dataMenu="place" color="darkgreen" marginTop="-45px" marginLeft="10px" />
 					</div>
 					<div className="row cell cell-10 center-block">
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="h" dataMenu="mountain" color="yellow" marginTop="20px" marginLeft="10px" />
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="i" dataMenu="mountain" color="lightgreen" marginTop="10px" marginLeft="-30px" />
+						<Dot data={data["h"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="h" dataMenu="mountain" color="yellow" marginTop="20px" marginLeft="10px" />
+						<Dot data={data["i"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="i" dataMenu="mountain" color="lightgreen" marginTop="10px" marginLeft="-30px" />
 					</div>
 					<div className="row cell cell-20"></div>
 					
@@ -78,7 +79,7 @@ var Content = React.createClass({
 					</div>
 					<div className="row cell cell-10"></div>
 					<div className="row cell cell-10 center-block">
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="j" dataMenu="water" color="darkred" marginTop="25px" marginLeft="0px" />
+						<Dot data={data["j"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="j" dataMenu="water" color="darkred" marginTop="25px" marginLeft="0px" />
 					</div>
 					<div className="row cell cell-10"></div>
 					<div className="row cell cell-5"></div>
@@ -97,22 +98,22 @@ var Content = React.createClass({
 					<div className="row cell cell-20"></div>
 					<div className="row cell cell-10"></div>
 					<div className="row cell cell-10 center-block">
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="k" dataMenu="place" color="gray" marginTop="7px" marginLeft="9px" />
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="l" dataMenu="place" color="black" marginTop="-12px" marginLeft="-23px" />
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="n" dataMenu="place" color="orange" marginTop="-35px" marginLeft="70px" />
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="o" dataMenu="place" color="lightblue" marginTop="0px" marginLeft="40px" />
+						<Dot data={data["k"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="k" dataMenu="place" color="gray" marginTop="7px" marginLeft="9px" />
+						<Dot data={data["l"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="l" dataMenu="place" color="black" marginTop="-12px" marginLeft="-23px" />
+						<Dot data={data["n"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="n" dataMenu="place" color="orange" marginTop="-35px" marginLeft="70px" />
+						<Dot data={data["o"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="o" dataMenu="place" color="lightblue" marginTop="0px" marginLeft="40px" />
 					</div>
 					<div className="row cell cell-20"></div>
 				</div>
 			  <div className="col-xs-1 topcol topcol-8">
 					<div className="row cell cell-20"></div>
 					<div className="row cell cell-20">
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="p" dataMenu="water" color="darkgray" marginTop="-5px" marginLeft="35px" />
+						<Dot data={data["p"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="p" dataMenu="water" color="darkgray" marginTop="-5px" marginLeft="35px" />
 					</div>
 					<div className="row cell cell-10">
 					</div>
 					<div className="row cell cell-20 center-block">
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="m" dataMenu="mountain" color="purple" marginTop="-10px" marginLeft="-85px" />
+						<Dot data={data["m"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="m" dataMenu="mountain" color="purple" marginTop="-10px" marginLeft="-85px" />
 					</div>
 					<div className="row cell cell-5"></div>
 					<div className="row cell cell-10">
@@ -143,14 +144,14 @@ var Content = React.createClass({
 					</div>
 					<div className="row cell cell-5"></div>
 					<div className="row cell cell-10">
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="q" dataMenu="water" color="darkblue" marginTop="-10px" marginLeft="-50px" />
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="r" dataMenu="place" color="darkgray2" marginTop="40px" marginLeft="-80px" />
+						<Dot data={data["q"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="q" dataMenu="water" color="darkblue" marginTop="-10px" marginLeft="-50px" />
+						<Dot data={data["r"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="r" dataMenu="place" color="darkgray2" marginTop="40px" marginLeft="-80px" />
 					</div>
 					<div className="row cell cell-20">
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="s" dataMenu="place" color="lightgreen2" marginTop="20px" marginLeft="-30px" />
+						<Dot data={data["s"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="s" dataMenu="place" color="lightgreen2" marginTop="20px" marginLeft="-30px" />
 					</div>
 					<div className="row cell cell-20">
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="t" dataMenu="place" color="weirdorange" marginTop="0px" marginLeft="-30px" />
+						<Dot data={data["t"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="t" dataMenu="place" color="weirdorange" marginTop="0px" marginLeft="-30px" />
 					</div>
 					<div className="row cell cell-10">
 						<div className="wave-blue-small"></div>
@@ -163,9 +164,9 @@ var Content = React.createClass({
 						<div className="wave-blue-small"></div>
 					</div>
 					<div className="row cell cell-20">
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="u" dataMenu="water" color="lightblue2" marginTop="10px" marginLeft="15px" />
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="v" dataMenu="island" color="darkgreen2" marginTop="15px" marginLeft="-5px" />
-						<Dot selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="w" dataMenu="mountain" color="lightbrown" marginTop="15px" marginLeft="-55px" />
+						<Dot data={data["u"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="u" dataMenu="water" color="lightblue2" marginTop="10px" marginLeft="15px" />
+						<Dot data={data["v"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="v" dataMenu="island" color="darkgreen2" marginTop="15px" marginLeft="-5px" />
+						<Dot data={data["w"]} selectedMenuItems={this.props.selectedMenuItems} onClick={this.props.onDotClick} label="w" dataMenu="mountain" color="lightbrown" marginTop="15px" marginLeft="-55px" />
 					</div>
 					<div className="row cell cell-10"></div>
 					<div className="row cell cell-20">
--- src/components/Dot.js
+++ src/components/Dot.js
@@ -9,57 +9,27 @@ var Dot = React.createClass({
 			displayToolTip: false
 		};
 	},
+
 	componentDidMount: function () {
 		this._enableTooltip();
 	},
+
 	componentDidUpdate: function () {
 		this._enableTooltip();
 	},
 
 	_enableTooltip: function () {
-  	$(React.findDOMNode(this)).tooltip();
+		var toolTipTemplate =
+			'<div class="tooltip top">' +
+				'<div class="tooltip-arrow tooltip-mine-arrow" style="border-top-color: ' + this.props.data.color + '"></div>' +
+				'<div class="tooltip-inner tooltip-mine" style="color: ' + this.props.data.color + '; background-image: url(\'assets/pages/' + this.props.data.label + '/bubble.png\')"></div>' +
+       '</div>';
+  	$(React.findDOMNode(this)).tooltip({ template: toolTipTemplate });
 	},
 
 	_isHidden: function () {
 		if (!this.props.selectedMenuItems[this.props.dataMenu]) return true;
 		return false;
-	},
-
-	_onMouseOver: function () {
-		this.setState({ displayToolTip: true });
-	},
-	_onMouseOut: function () {
-		this.setState({ displayToolTip: false });
-	},
-
-	_getToolTip: function () {
-		var arrowStyle = {
-			left: "50%"
-		};
-		var toolTip = null;
-		if (this.state.displayToolTip) {
-  		var node = React.findDOMNode(this.refs.dot);
-			var offset = $(node).offset();
-			var posY = offset.top - $(window).scrollTop();
-			var posX = offset.left - $(window).scrollLeft(); 
-			console.log(posX + " " + posY);
-  		var dotPositionA = React.findDOMNode(this).getClientRects();
-			var dotPosition = dotPositionA[1];
-			console.log(dotPosition);
-			var toolTipStyle = {
-				top: (posX - 150) + 'px',
-				left: (posY - 150) + 'px',
-				display: 'block'
-			};
-			console.log(toolTipStyle);
-			toolTip = (
-				<div className="tooltip fade top in" role="tooltip" style={toolTipStyle}>
-					<div className="tooltip-arrow" style={arrowStyle}></div>
-					<div className="tooltip-inner">.</div>
-				</div>
-			);
-		};
-		return toolTip;
 	},
 
 	render: function () {
@@ -69,12 +39,6 @@ var Dot = React.createClass({
 			marginTop: this.props.marginTop,
 			marginLeft: this.props.marginLeft
 		};
-		var bubble = null;
-		if (this.props.label === "c") {
-			bubble = (
-				<div className="bubble"></div>
-			);
-		}
 		var anchor = "#" + this.props.label;
 		return (
 			<a id={dotId} data-menu={this.props.dataMenu} ref="dot" title="." className={classes} href={anchor} style={dotStyle} onClick={this.props.onClick.bind(null, this.props.label)}></a>
